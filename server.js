a211a6f31772b6172b3c3fed6274cf08421bf8ae
brightowsus <owusub230@gmail.com>
2025-04-22 19:12:37 +0000

    update

--- server.js
+++ server.js
@@ -208,16 +208,7 @@ app.post('/api/user/profile', async (req, res) => {
     }
   });
 
-  
-//Fetch user info
-app.get('/api/user/:id', async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      res.json({ success: true, user });
-    } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
-    }
-});
+
 
   
 // ✅ Start Server
